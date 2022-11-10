00fca78438292ed9a85dd231e989a9af6b0acab9
Francis_MD <hifrancismd@gmail.com>
2022-11-10 13:24:02 +0400

    challenge 6 vowel count solved

--- Challenge06/site06.js
+++ Challenge06/site06.js
@@ -14,19 +14,20 @@ function findVowels() {
     
     //used for display no need to change
     //check if the obj returned is empty
-    /* if (Object.keys(vowelObj).length === 0){
+    if (Object.keys(vowelObj).length === 0){
         alert("Error: Vowel Object is Empty");
         return;
-    } */
+    }
 
-    document.getElementById("results").innerHTML = `You entered <strong>"${str}"</strong> it has ${vowelObj} vowels.`;
-
+    /* document.getElementById("results").innerHTML = `You entered <strong>"${str}"</strong> it has ${vowelObj} vowels.`; */
+    document.getElementById("results").innerHTML = `You entered <strong>"${str}"</strong> it has ${vowelObj.vCount} vowels.<br>The vowels found are ${vowelObj.vFound.toString()}`;
 }
 
 //takes an string and returns the vowel count and the vowels found as an object.
 function getVowelCount(str) {
     let vowelCount = 0;
     let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let foundVowels = [];
 
     str = str.toLowerCase();
 
@@ -34,11 +35,17 @@ function getVowelCount(str) {
         
         if (vowels.includes(str[index])){
             vowelCount ++;
+            foundVowels.push(str[index]);
         }
         
     }
 
+    let vowelobj = {
+        vCount : vowelCount,
+        vFound : foundVowels
+    }
+
     
-    return vowelCount;
+    return vowelobj;
 
 }
